fef9553ff809cf646c5c4073f77c19207c478c3d
PetTony <anton.petkelyov@yandex.ru>
2018-01-18 19:33:52 +0300

    Update Лабораторная2.cpp

--- Лаба 2/Лабораторная2.cpp
+++ Лаба 2/Лабораторная2.cpp
@@ -9,17 +9,16 @@ int main (){
 	setlocale(LC_ALL, "rus");
 	int len; 
 	char *code = new char[1000];
-	printf("Введите ключ: ");
+	printf("Р’РІРµРґРёС‚Рµ РєР»СЋС‡: ");
 	scanf("%s", code);
 	len=strlen(code);
-	//printf("Длина: %d", len);
 	FILE *fp1, *fp2, *fp3;
 	fp1=fopen("input.txt", "r");
 	fp2=fopen("encrypt.txt", "w");
 	fp3=fopen("decrypt.txt", "w");
 	int flag;
     char point;
-    int circle=0; // круг
+    int circle=0; // РєСЂСѓРі
     point=getc(fp1);
     while (!feof(fp1)){
     		if (circle<=len){
@@ -34,10 +33,10 @@ int main (){
     			fprintf(fp2, "%c", point);    			
         	point = getc(fp1);
     }
-    //дешифрация
+    //РґРµС€РёС„СЂР°С†РёСЏ
     fclose (fp2);
     fp2=fopen("encrypt.txt", "r");
-    circle=0; // круг
+    circle=0; // РєСЂСѓРі
     point=getc(fp2);
     while (!feof(fp2)){
     		if (circle<=len){
